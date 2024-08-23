856e8251822e632bf41342566190d8d6d9059513
김영후 <eagle7449@naver.com>
2024-08-23 14:56:07 +0900

    변수명 오타 수정

--- hoo/august/PGMS_240823_숫자변환하기_BFS.java
+++ hoo/august/PGMS_240823_숫자변환하기_BFS.java
@@ -4,7 +4,7 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Queue;
 
-public class PGMS_240823_숫자변환하기 {
+public class PGMS_240823_숫자변환하기_BFS {
 
     public int solution(int x, int y, int n) {
         int answer = calcCase(x, y, n);
@@ -13,28 +13,28 @@ public class PGMS_240823_숫자변환하기 {
     }
 
     int calcCase(int x, int y, int n) {
-        int[] isMaked = new int[y+1];   // y까지 숫자를 만들며 들었던 최소 횟수를 저장
-        Arrays.fill(isMaked, Integer.MAX_VALUE);    // 횟수는 우선 최댓값으로 초기화
+        int[] isMade = new int[y+1];   // y까지 숫자를 만들며 들었던 최소 횟수를 저장
+        Arrays.fill(isMade, Integer.MAX_VALUE);    // 횟수는 우선 최댓값으로 초기화
 
         Queue<int[]> q = new ArrayDeque<>();    // 0번 인덱스에는 숫자, 1번 인덱스에는 횟수 저장
         q.offer(new int[] {x, 0});
-        isMaked[x] = 0; // x를 만드는 데 필요한 연산은 없으므로 0으로 갱신
+        isMade[x] = 0; // x를 만드는 데 필요한 연산은 없으므로 0으로 갱신
 
         int nowNumber;
         while (!q.isEmpty()) {
             int[] now = q.poll();
             nowNumber = now[0];
-            if (isMaked[nowNumber] < now[1]) continue;  // 이미 더 적은 횟수로 해당 숫자를 만들었으면 건너 뜀
+            if (isMade[nowNumber] < now[1]) continue;  // 이미 더 적은 횟수로 해당 숫자를 만들었으면 건너 뜀
 
             for (int i = 0; i < 3; i++) {
                 int makedNumber = makeNextNumber(i, n, nowNumber);
-                if (makedNumber > y || isMaked[makedNumber] < now[1] + 1) continue;    // 이미 더 적은 횟수로 해당 숫자를 만들었으면 건너 뜀
+                if (makedNumber > y || isMade[makedNumber] < now[1] + 1) continue;    // 이미 더 적은 횟수로 해당 숫자를 만들었으면 건너 뜀
                 q.offer(new int[] {makedNumber, now[1] + 1});
-                isMaked[makedNumber] = now[1] + 1;
+                isMade[makedNumber] = now[1] + 1;
             }
         }
 
-        return (isMaked[y] == Integer.MAX_VALUE)? -1:isMaked[y];    // 숫자를 만들 수 없었으면 -1, 만들 수 있으면 저장된 최솟값 출력
+        return (isMade[y] == Integer.MAX_VALUE)? -1:isMade[y];    // 숫자를 만들 수 없었으면 -1, 만들 수 있으면 저장된 최솟값 출력
     }
 
     int makeNextNumber(int mode, int n, int nowNumber) {
