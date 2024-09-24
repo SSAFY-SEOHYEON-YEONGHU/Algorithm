4c503a8d83192eb2ec2cf78c98c2a68ff9bd3265
김영후 <eagle7449@naver.com>
2024-09-24 13:59:11 +0900

    연속 부분 수열의 합의 개수 N^2으로 수정

--- hoo/september/week4/PGMS_240924_연속부분수열합의개수.java
+++ hoo/september/week4/PGMS_240924_연속부분수열합의개수.java
@@ -8,7 +8,6 @@ public class PGMS_240924_연속부분수열합의개수 {
     public int solution(int[] elements) {
         int answer = calcAnswer(elements);
 
-
         return answer;
     }
 
@@ -16,11 +15,13 @@ public class PGMS_240924_연속부분수열합의개수 {
         Set<Integer> sumSet = new HashSet<>();
         int sum;
         for (int i = 1; i <= elements.length; i++) { // elements의 길이만큼의 원소를 선택
-            for (int j = 0; j < elements.length; j++) { // elements의 처음부터 끝 원소까지
-                sum = 0;
-                for (int k = 0; k < i; k++) {  // 연속으로 i개를 선택해서 더해줌
-                    sum += elements[(j+k) % elements.length];
-                }
+            sum = 0;
+            for (int j = 0; j < i; j++) sum += elements[j];   // 우선 맨 첫수부터 연속한 i개를 모두 선택해서 만들 수 있는 합을 만들어 줌
+            sumSet.add(sum);
+
+            for (int j = 0; j < elements.length; j++) { // 이후 슬라이딩 윈도우 형식으로 옮겨가며 바뀐 연속 숫자들의 합을 계산
+                sum -= elements[j];
+                sum += elements[(j+i) % elements.length];
                 sumSet.add(sum);
             }
         }
