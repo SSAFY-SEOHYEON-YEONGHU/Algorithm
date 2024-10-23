829d86acbfefbc3c1063e33784ab8ded9daa2dfa
박서현 <mentos0601@naver.com>
2024-10-23 16:33:23 +0900

    PGMS_241023_2_실패코드

--- hyun/10_october/PGMS_241023_디펜스게임.java
+++ hyun/10_october/PGMS_241023_디펜스게임.java
@@ -11,6 +11,12 @@ class Solution {
     }
     public void simulation(int n, int k, int[] enemy){
         Queue<Node> q = new ArrayDeque<>();
+        int[][] visited = new int[enemy.length+1][k+1]; // 라운드, k사용횟수
+        for(int i=0; i<enemy.length; i++)
+            for(int j=0;j<=k;j++)
+                visited[i][j] = -1;
+
+        visited[0][k] = n;
         q.add(new Node(n,k));
 
         for(int i=0; i<enemy.length;i++){
@@ -18,15 +24,24 @@ class Solution {
             for(int j=0; j<size; j++){
                 Node cur = q.poll();
                 if(cur.n <= 0) {
-                    answer = Math.max(answer,i-1);
+                    answer = Math.max(answer,i);
                     continue;
                 }
                 if(cur.k > 0){
-                    q.add(new Node(cur.n, cur.k-1));
-                    q.add(new Node(cur.n - enemy[i], cur.k));
+                    if(visited[i+1][cur.k-1] < cur.n ){
+                        visited[i+1][cur.k-1] = cur.n;
+                        q.add(new Node(cur.n, cur.k-1));
+                    }
+                    if(visited[i+1][cur.k] < cur.n-enemy[i]){
+                        visited[i+1][cur.k] = cur.n-enemy[i];
+                        q.add(new Node(cur.n - enemy[i], cur.k));
+                    }
                 }
                 else{
-                    q.add(new Node(cur.n - enemy[i], 0));
+                    if(visited[i+1][0] < cur.n-enemy[i]){
+                        visited[i+1][0] = cur.n-enemy[i];
+                        q.add(new Node(cur.n - enemy[i], 0));
+                    }
                 }
             }
         }
