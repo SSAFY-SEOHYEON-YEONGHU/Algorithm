b019a324a608fc3fbaa2fb0cc723f83605ed8b50
박서현 <mentos0601@naver.com>
2025-01-03 17:29:55 +0900

    BOJ_250103_계란으로 계란치기

---
+++ hyun/1_January/BOJ_250103_계란으로계란치기.java
@@ -0,0 +1,60 @@
+package backtracking;
+
+import java.io.*;
+import java.util.*;
+public class BOJ_250103_계란으로계란치기 {
+    static int N;
+    static int[][] eggs;
+    static int answer = 0;
+    static class Node{
+        int hard, weight;
+        boolean broke;
+        Node(int hard, int weight, boolean broke){
+            this.hard = hard;
+            this.weight = weight;
+            this.broke = broke;
+        }
+    }
+
+    static void start(int cnt){
+        if(cnt == N) {
+            // 갯수 세기
+            int sum = 0;
+            for(int i=0; i<N; i++) if(eggs[i][0] <= 0) sum++;
+            answer = Math.max(answer,sum);
+            return;
+        }
+
+        if(eggs[cnt][0] > 0) {
+            for (int i = 0; i < N; i++) {
+                if (i == cnt || eggs[i][0] <= 0) continue;
+
+                eggs[cnt][0] -= eggs[i][1];
+                eggs[i][0] -= eggs[cnt][1];
+
+                start(cnt + 1);
+
+                eggs[cnt][0] += eggs[i][1];
+                eggs[i][0] += eggs[cnt][1];
+            }
+
+        }
+        else start(cnt+1);
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        eggs = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            eggs[i][0] = Integer.parseInt(st.nextToken());
+            eggs[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        start(0);
+        System.out.println(answer);
+    }
+}
